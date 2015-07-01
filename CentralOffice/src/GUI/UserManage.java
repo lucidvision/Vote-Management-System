e4b4ac7051febc187e3f1f8f648408acc42d125e
lucidvision <brianpark25@gmail.com>
2015-06-30 17:35:01 -0700

    Update UserManage.java

--- CentralOffice/src/GUI/UserManage.java
+++ CentralOffice/src/GUI/UserManage.java
@@ -4,9 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 import ManageUsers.*;
-/**
- * @author Bruce Yi
- */
+
 public class UserManage extends JFrame implements ActionListener{
 	private UserManagement UserManager;
 	private int InitialWidth  = 500;
